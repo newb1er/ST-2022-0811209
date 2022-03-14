d7cb03ad603521cd923a6edf8f990014261cfa18
newb1er <taiwanmichael429@gmail.com>
2022-03-14 23:52:29 +0800

    exception test added

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -1,12 +1,15 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PriorityQueueTest {
     static Stream<Arguments> streamProvider() {
@@ -16,7 +19,9 @@ public class PriorityQueueTest {
                 Arguments.of(new int[] {1,2,3,4,5}, new int[] {1,2,3,4,5}),
                 Arguments.of(new int[] {5,4,3,2,1}, new int[] {1,2,3,4,5}),
                 Arguments.of(new int[] {4,1,7,3,2}, new int[] {1,2,3,4,7}),
-                Arguments.of(new int[] {1}, new int[] {1})
+                Arguments.of(new int[] {0, -1, 1}, new int[] {-1, 0, 1}),
+                Arguments.of(new int[] {1}, new int[] {1}),
+                Arguments.of(new int[] {}, new int[] {})
         );
     }
 
@@ -28,20 +33,49 @@ public class PriorityQueueTest {
         Integer s;
         int[] result = new int[random_array.length];
 
-        // TODO: random_array add to PriorityQueue
-
         for (int i = 0; i < random_array.length; i++) {
             test.add(random_array[i]);
         }
-
-        // TODO: get PriorityQueue result
 
         for (int i = 0; i < random_array.length; i++) {
             result[i] = test.poll();
         }
 
         assertArrayEquals(correct_array, result);
+    }
 
-        // TODO: 3 unique Exceptions
+    @Test
+    public void constructor_InitialCapacityLessThanOne() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new PriorityQueue<>(0);
+        });
+    }
+
+    @Test
+    public void constructor_CollectionBeNull() {
+        assertThrows(NullPointerException.class, () -> {
+            Integer i = null;
+            new PriorityQueue<Integer>(i);
+        });
+    }
+
+    @Test
+    public void add_ElementBeNull() {
+        assertThrows(NullPointerException.class, () -> {
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            Integer i = null;
+
+            pq.add(i);
+        });
+    }
+
+    @Test
+    public void offer_ElementBeNull() {
+        assertThrows(NullPointerException.class, () -> {
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            Integer i = null;
+
+            pq.add(i);
+        });
     }
 }
